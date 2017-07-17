8aab4bf45757fe2f5951ae4d0a379d9cd476b07c
Luke Larsen <lukejet7@gmail.com>
2017-07-16 20:45:01 -0700

    All previous files
    This is an old spammer file I made along time ago that I might work on
    here and there. This is all of the original files.

---
+++ spamer/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Timer1.Interval = TextBox2.Text 'Sets the interval at the timer1
+        Timer1.Enabled = True 'Enables timer1
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Timer1.Interval = TextBox2.Text 'This command is not necessary
+        Timer1.Enabled = False 'This stops timer1 repeating the command
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        SendKeys.Send(TextBox1.Text) 'Sends the message you typed in the textbox1
+        SendKeys.Send("{enter}") 'presses the ENTER key from your keyboard
+    End Sub
+End Class
